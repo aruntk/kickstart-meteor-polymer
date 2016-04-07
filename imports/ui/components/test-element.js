bf93bd1aa886263b2f1998ca48bf35b53e4e31da
tkay <aruntknambiar@gmail.com>
2016-04-07 13:54:49 +0530

    route fix

--- imports/ui/components/test-element.js
+++ imports/ui/components/test-element.js
@@ -1,32 +1,29 @@
 import './test-element.html';
-Synthesizer.ready(function(){
-
-  Polymer({
-    is:"test-element",
-    properties:{
-      name:{
-        type:String,
-        value:"Arun Kumar"
-      },
-      nickname:{
-        type:String,
-        value:"tkay"
-      },
-      show:{
-        type:String,
-        value:"show"
-      },
-      nndHidden:{
-        type:Boolean,
-        value:true
-      }
+Synthesis({
+  is:"test-element",
+  properties:{
+    name:{
+      type:String,
+      value:"Arun Kumar"
     },
-    showNickName:function(){
-      this.nndHidden = !this.nndHidden;
-      this.show = this.nndHidden ? "show" : "hide";
+    nickname:{
+      type:String,
+      value:"tkay"
+    },
+    show:{
+      type:String,
+      value:"show"
+    },
+    nndHidden:{
+      type:Boolean,
+      value:true
     }
+  },
+  showNickName:function(){
+    this.nndHidden = !this.nndHidden;
+    this.show = this.nndHidden ? "show" : "hide";
+  }
 
-  })
+})
 
 
-});
--- imports/ui/layouts/test-layout.js
+++ imports/ui/layouts/test-layout.js
@@ -1,30 +1,29 @@
 import './test-layout.html';
 
-Synthesizer.ready(function(){
-  Polymer({
-    is:"test-layout",
-    behaviors:[mwcMixin,mwcRouter],
-    getMeteorData:function(){
-      this.set("status",Meteor.status().status);
+Synthesis({
+  is:"test-layout",
+  behaviors:[mwcMixin,mwcRouter],
+  getMeteorData:function(){
+    this.set("status",Meteor.status().status);
+  },
+  properties:{
+    mwcRoute:{
+      type:Object,
+      name:"landing",
+      params:{"view":"home"}
     },
-    properties:{
-      mwcRoute:{
-        type:Object,
-        name:"landing",
-        params:{"view":"home"}
-      },
-      status:{
-        type:String
-      }
-
-    },
-    second:function(){
-      this.set("mwcRoute.params.view", "second"); 
-    },
-    home:function(){
-
-      this.set("mwcRoute.params.view", "home"); 
+    status:{
+      type:String
     }
-  });
 
+  },
+  second:function(){
+    this.set("mwcRoute.params.view", "second"); 
+  },
+  home:function(){
+
+    this.set("mwcRoute.params.view", "home"); 
+  }
 });
+
+
